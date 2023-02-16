1facf5e1443027722a2c038138a331db0936180c
GardnerGithub <94706853+GardnerGithub@users.noreply.github.com>
2023-02-15 19:50:55 -0800

    Version 3: 02/15/23
    Renamed file from GPS_Coord_To_Distance --> GPS_Update_Function
    
    Condensed file to a single tab.

--- GPS_Update_Function/GPS_Update_Function.ino
+++ GPS_Update_Function/GPS_Update_Function.ino
@@ -1,4 +1,18 @@
 /*
+ * Converting GPS Latitude and Longitude to distance to drive
+ */
+volatile float LAT_DIFF = 0; // initialize latitude difference variable
+volatile float LONG_DIFF = 0; // initialize longitude difference variable
+volatile float A = 0; // initialize argument of the square root
+volatile float C = 0; // initialize arctangent term
+volatile float DISTANCE = 0; // initialize final distance variable
+volatile float TARGET_HEADING = 0; // initialize target heading
+/*
+ * VOLATILE FLOAT --> changing number with a lot of decimal points
+ * CONST DOUBLE --> unchanging number of double variable type
+ */
+
+/*
  * Converting GPS coordinates to distance to drive and heading
  */
  
@@ -22,3 +36,19 @@ void GPSUpdate(float initial_lat, float initial_long, float final_lat, float fin
   TARGET_HEADING = atan2(sin(LONG_DIFF)*cos(final_lat), 
   (cos(initial_lat)*sin(final_lat)) - (sin(initial_lat)*cos(final_lat)*cos(LONG_DIFF)));
  }
+
+ // On-board LED to know when program has been successfully downloaded to board
+void LEDSetup(){
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH); // turn on light to ensure board is working
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(DISTANCE);
+}
