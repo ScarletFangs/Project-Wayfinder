d43d5a818131fa2224e04d2eb4615f62c31ad81e
GardnerGithub <94706853+GardnerGithub@users.noreply.github.com>
2023-02-15 19:39:32 -0800

    Version 2: 02/15/23 GG
    Changed DriveToGPS() --> GPSUpdate()
    
    Moved initialization of global variables to principle file.

--- GPS_Coord_To_Distance/B_GPS_To_Distance.ino
+++ GPS_Coord_To_Distance/B_GPS_To_Distance.ino
@@ -1,18 +1,8 @@
 /*
  * Converting GPS coordinates to distance to drive and heading
  */
-volatile float LAT_DIFF = 0; // initialize latitude difference variable
-volatile float LONG_DIFF = 0; // initialize longitude difference variable
-volatile float A = 0; // initialize argument of the square root
-volatile float C = 0; // initialize arctangent term
-volatile float DISTANCE = 0; // initialize final distance variable
-volatile float TARGET_HEADING = 0; // initialize target heading
-/*
- * VOLATILE FLOAT --> changing number with a lot of decimal points
- * CONST DOUBLE --> unchanging number of double variable type
- */
  
-void DriveToGPS(float initial_lat, float final_lat, float initial_long, float final_long){
+void GPSUpdate(float initial_lat, float initial_long, float final_lat, float final_long){
   /*
    * Uses haversine formula to calculate distance between two points when given latitudes and longitudes
    */
--- GPS_Coord_To_Distance/GPS_Coord_To_Distance.ino
+++ GPS_Coord_To_Distance/GPS_Coord_To_Distance.ino
@@ -1,6 +1,16 @@
 /*
  * Converting GPS Latitude and Longitude to distance to drive
  */
+volatile float LAT_DIFF = 0; // initialize latitude difference variable
+volatile float LONG_DIFF = 0; // initialize longitude difference variable
+volatile float A = 0; // initialize argument of the square root
+volatile float C = 0; // initialize arctangent term
+volatile float DISTANCE = 0; // initialize final distance variable
+volatile float TARGET_HEADING = 0; // initialize target heading
+/*
+ * VOLATILE FLOAT --> changing number with a lot of decimal points
+ * CONST DOUBLE --> unchanging number of double variable type
+ */
 #include <Servo.h>
 
 void setup() {
@@ -12,5 +22,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.println(DISTANCE);
 }
